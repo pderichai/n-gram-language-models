d33001f5eae1c455be263b968e07250b93931b2b
Deric Pang <dericp@cs.washington.edu>
2017-01-19 02:40:39 -0800

    Finished making data splits.

--- python/split_data.py
+++ python/split_data.py
@@ -3,7 +3,7 @@ import random
 
 lines = list()
 
-with open('data/brown.txt') as f:
+with open('data/gutenberg.txt', encoding='iso-8859-1') as f:
     for line in f:
         lines.append(line)
     f.close()
@@ -11,17 +11,40 @@ with open('data/brown.txt') as f:
 indices = list(range(len(lines)))
 random.shuffle(indices)
 
-with open('data/brown_dev.txt', 'w') as f:
+with open('data/gutenberg_dev.txt', 'w', encoding='iso-8859-1') as f:
     for i in range(int(0.1 * len(lines))):
         f.write(lines[indices[i]])
     f.close()
 
-with open('data/brown_test.txt', 'w') as f:
+with open('data/gutenberg_test.txt', 'w', encoding='iso-8859-1') as f:
     for i in range(int(0.1 * len(lines)), int(0.2 * len(lines))):
         f.write(lines[indices[i]])
     f.close()
 
-with open('data/brown_train.txt', 'w') as f:
+with open('data/gutenberg_train.txt', 'w', encoding='iso-8859-1') as f:
     for i in range(int(0.2 * len(lines)), len(lines)):
         f.write(lines[indices[i]])
     f.close()
+
+'''with open('data/reuters.txt', encoding='iso-8859-1') as f:
+    for line in f:
+        lines.append(line)
+    f.close()
+
+indices = list(range(len(lines)))
+random.shuffle(indices)
+
+with open('data/reuters_dev.txt', 'w', encoding='iso-8859-1') as f:
+    for i in range(int(0.1 * len(lines))):
+        f.write(lines[indices[i]])
+    f.close()
+
+with open('data/reuters_test.txt', 'w', encoding='iso-8859-1') as f:
+    for i in range(int(0.1 * len(lines)), int(0.2 * len(lines))):
+        f.write(lines[indices[i]])
+    f.close()
+
+with open('data/reuters_train.txt', 'w', encoding='iso-8859-1') as f:
+    for i in range(int(0.2 * len(lines)), len(lines)):
+        f.write(lines[indices[i]])
+    f.close()'''
